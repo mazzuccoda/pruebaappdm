96b47601b151afc41b6f2b7fc97d284892b01a12
mazzuccoda <73189806+mazzuccoda@users.noreply.github.com>
2022-01-11 01:59:13 -0300

    s
    s

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ if __name__ == '__main__':
     print('Inove@Server start!')
 
     # Lanzar server
-    app.run(host="127.0.0.1", port=5000)
+    #app.run(host="127.0.0.1", port=5000)
+    
+    app.run()
